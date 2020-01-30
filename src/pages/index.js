be327998bd26c33b32370c91b4c163d5a02157c8
stormy Adams <stormy251251@gmail.com>
2020-01-29 21:53:59 -0800

    Clean up the dummy modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,21 @@ import Link from 'next/link';
 import {ModalContext} from 'lib/contexts/ModalContext';
 import DummyLayout from 'components/layouts/DummyLayout';
 import Modal from 'components/Modal';
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+	background-color: #ffffff;
+	border-radius: 8px;
+	padding: 8px;
+`;
 
 const DummyModal = ({closeModal}) => {
 	return (
 		<Modal>
-			<h1>Hi there I am a modal</h1>
-			<button onClick={closeModal}>Close Modal</button>
+			<ModalContainer>
+				<h1>Hi there I am a modal</h1>
+				<button onClick={closeModal}>Close Modal</button>
+			</ModalContainer>
 		</Modal>
 	);
 };
